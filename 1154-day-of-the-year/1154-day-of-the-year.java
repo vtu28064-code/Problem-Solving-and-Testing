40e9aaaf976fdc17551fc5a88e1d8255759c8922
vtu28064-code <vtu28064@veltech.edu.in>
2026-09-09 09:14:52 +0530

    Time: 30 ms (8.14%), Space: 48.8 MB (6.15%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+class Solution {
+    public int dayOfYear(String date) {
+        LocalDate localDate = LocalDate.parse(date);
+        return localDate.getDayOfYear();
+    }
+}
