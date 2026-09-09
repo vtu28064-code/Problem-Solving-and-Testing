0b8bc88ac493aef160ce4fe599de395a8d717606
vtu28064-code <vtu28064@veltech.edu.in>
2026-09-09 09:27:38 +0530

    Time: 0 ms (100%), Space: 46.9 MB (28.55%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        
+        // The dimensions swap: rows become columns, columns become rows
+        int[][] ans = new int[c][r];
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        
+        return ans;
+    }
+}
